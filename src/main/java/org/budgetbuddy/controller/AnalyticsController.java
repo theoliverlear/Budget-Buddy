3848ad0d2b0facead189d36895fdfb70ffb589b7
theoliverlear <sigwartholiver2@gmail.com>
2024-03-03 19:22:27 -0600

    Add AnalyticsController.java to control webpages related to analytics data

---
+++ src/main/java/org/budgetbuddy/controller/AnalyticsController.java
@@ -0,0 +1,35 @@
+package org.budgetbuddy.controller;
+//=================================-Imports-==================================
+import org.budgetbuddy.service.AnalyticsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/analytics")
+public class AnalyticsController extends BudgetBuddyController {
+    //============================-Variables-=================================
+    @Autowired
+    AnalyticsService analyticsService;
+    //===========================-Constructors-===============================
+
+    //=============================-Methods-==================================
+
+    //-----------------------------Analytics----------------------------------
+    @RequestMapping("/")
+    public String analytics() {
+        return "analytics";
+    }
+
+    //============================-Overrides-=================================
+
+    //------------------------------Equals------------------------------------
+
+    //------------------------------Hash-Code---------------------------------
+
+    //------------------------------To-String---------------------------------
+
+    //=============================-Getters-==================================
+
+    //=============================-Setters-==================================
+}
