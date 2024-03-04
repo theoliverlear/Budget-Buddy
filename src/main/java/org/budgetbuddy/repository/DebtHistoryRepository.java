10120af0340e6671faff820011b9bc0d26ead58d
theoliverlear <sigwartholiver2@gmail.com>
2024-03-04 11:46:41 -0600

    Add header comments to DebtHistoryRepository.java

--- src/main/java/org/budgetbuddy/repository/DebtHistoryRepository.java
+++ src/main/java/org/budgetbuddy/repository/DebtHistoryRepository.java
@@ -1,4 +1,8 @@
 package org.budgetbuddy.repository;
+//=================================-Imports-==================================
+import org.budgetbuddy.entity.debt.DebtHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DebtHistoryRepository {
+public interface DebtHistoryRepository extends JpaRepository<DebtHistory, Long> {
+    //=============================-Methods-==================================
 }
