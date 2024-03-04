d1f4fe85620f1219c9368d12757eab3e12b518e4
theoliverlear <sigwartholiver2@gmail.com>
2024-03-04 11:40:12 -0600

    Add header comments to PurchaseHistoryRepository.java

--- src/main/java/org/budgetbuddy/repository/PurchaseHistoryRepository.java
+++ src/main/java/org/budgetbuddy/repository/PurchaseHistoryRepository.java
@@ -1,7 +1,8 @@
 package org.budgetbuddy.repository;
-
+//=================================-Imports-==================================
 import org.budgetbuddy.entity.purchase.PurchaseHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PurchaseHistoryRepository extends JpaRepository<PurchaseHistory, Long> {
+    //=============================-Methods-==================================
 }
