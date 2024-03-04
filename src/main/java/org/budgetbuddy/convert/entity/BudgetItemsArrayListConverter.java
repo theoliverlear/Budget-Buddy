6d9268e44e112b2680fa4d0e64fcfeedaa91e4b4
theoliverlear <sigwartholiver2@gmail.com>
2024-03-03 19:45:36 -0600

    Add BudgetItemsArrayListConverter.java to convert ArrayLists of BudgetItems for the database

---
+++ src/main/java/org/budgetbuddy/convert/entity/BudgetItemsArrayListConverter.java
@@ -0,0 +1,46 @@
+package org.budgetbuddy.convert.entity;
+//=================================-Imports-==================================
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import org.budgetbuddy.entity.budget.BudgetItem;
+
+import java.util.ArrayList;
+
+@Converter(autoApply = true)
+public class BudgetItemsArrayListConverter implements AttributeConverter<ArrayList<BudgetItem>, String> {
+    //============================-Variables-=================================
+    ObjectMapper objectMapper = new ObjectMapper();
+    //===========================-Constructors-===============================
+    public BudgetItemsArrayListConverter() {
+        this.objectMapper = new ObjectMapper();
+    }
+    //============================-Methods-===================================
+
+    //---------------------Convert-To-Database-Column-------------------------
+    @Override
+    public String convertToDatabaseColumn(ArrayList<BudgetItem> budgetItems) {
+        try {
+            return objectMapper.writeValueAsString(budgetItems);
+        } catch (JsonProcessingException ex) {
+            final String EXCEPTION_MESSAGE = "Error converting budget items to JSON.";
+            throw new RuntimeException(EXCEPTION_MESSAGE, ex);
+        }
+    }
+    //--------------------Convert-From-Database-Column------------------------
+    @Override
+    public ArrayList<BudgetItem> convertToEntityAttribute(String budgetItemsJson) {
+        try {
+            if (budgetItemsJson == null) {
+                return new ArrayList<>();
+            }
+            return objectMapper.readValue(budgetItemsJson, new TypeReference<>() {});
+        } catch (JsonProcessingException ex) {
+            final String EXCEPTION_MESSAGE = "Error converting JSON to budget items.";
+            throw new RuntimeException(EXCEPTION_MESSAGE, ex);
+        }
+    }
+
+}
