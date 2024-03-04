e946d08d1240f8ab05a1a75804e47e3b511d4b7e
theoliverlear <sigwartholiver2@gmail.com>
2024-03-04 12:36:05 -0600

    Add security filter chain method to bypass Spring default login in SecurityConfig.java

--- src/main/java/org/budgetbuddy/config/SecurityConfig.java
+++ src/main/java/org/budgetbuddy/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package org.budgetbuddy.config;
 //=================================-Imports-==================================
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +18,18 @@ public class SecurityConfig {
 
     //==============================-Beans-===================================
 
+
+    //------------------------Security-Filter-Chain---------------------------
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // HttpSecurity is the builder object used to create the security filter chain
+        http.authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .csrf()
+                .disable();
+        return http.build();
+    }
     //--------------------------Password-Encoder------------------------------
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
