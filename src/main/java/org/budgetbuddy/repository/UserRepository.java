5340820702bf038625e2f58f11b594e7bdb1161e
theoliverlear <sigwartholiver2@gmail.com>
2024-03-03 22:28:46 -0600

    Add UserRepository.java to retrieve User.java files from the database

--- src/main/java/org/budgetbuddy/repository/UserRepository.java
+++ src/main/java/org/budgetbuddy/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package org.budgetbuddy.repository;
-
-import org.budgetbuddy.entity.User;
+//=================================-Imports-==================================
+import org.budgetbuddy.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    //=============================-Methods-==================================
     User findByUsername(String username);
     User getUserById(Long id);
 }
