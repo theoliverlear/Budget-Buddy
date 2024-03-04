05cb3614aa4cec21b2a5973c55c0487ab4baa5de
theoliverlear <sigwartholiver2@gmail.com>
2024-03-04 11:43:45 -0600

    Add header comments to ExpenseHistoryRepository.java

--- src/main/java/org/budgetbuddy/repository/ExpenseHistoryRepository.java
+++ src/main/java/org/budgetbuddy/repository/ExpenseHistoryRepository.java
@@ -1,7 +1,8 @@
 package org.budgetbuddy.repository;
-
+//=================================-Imports-==================================
 import org.budgetbuddy.entity.expense.ExpenseHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpenseHistoryRepository extends JpaRepository<ExpenseHistory, Long> {
+    //=============================-Methods-==================================
 }
