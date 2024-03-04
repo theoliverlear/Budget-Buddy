e08789f29e782c5cf6e42896af5a9fdd7de20f00
theoliverlear <sigwartholiver2@gmail.com>
2024-03-03 19:51:36 -0600

    Add SimpleInterest.java to represent simple interest accrued on Saving.java or Debt.java

---
+++ src/main/java/org/budgetbuddy/entity/interest/SimpleInterest.java
@@ -0,0 +1,46 @@
+package org.budgetbuddy.entity.interest;
+//=================================-Imports-==================================
+import jakarta.persistence.Entity;
+import org.budgetbuddy.entity.debt.Debt;
+import org.budgetbuddy.entity.savings.Saving;
+import org.budgetbuddy.entity.time.TimeInterval;
+
+@Entity
+public class SimpleInterest extends Interest {
+    //============================-Variables-=================================
+
+    //===========================-Constructors-===============================
+
+    //=============================-Methods-==================================
+
+    //============================-Overrides-=================================
+
+    //-------------------------Calculate-Interest-----------------------------
+    @Override
+    public double calculateInterest(double principal, TimeInterval timeInterval) {
+        // TODO: Implement cost of simple interest. Only include interest on
+        //       principal.
+        return 0;
+    }
+    //------------------------Add-Interest-To-Debt----------------------------
+    @Override
+    public Debt addInterestToDebt(Debt debt) {
+        // TODO: Implement adding simple interest to debt.
+        return null;
+    }
+    //-----------------------Add-Interest-To-Saving---------------------------
+    @Override
+    public Saving addInterestToSaving(Saving saving) {
+        // TODO: Implement adding simple interest to saving.
+        return null;
+    }
+    //------------------------------Equals------------------------------------
+
+    //------------------------------Hash-Code---------------------------------
+
+    //------------------------------To-String---------------------------------
+
+    //=============================-Getters-==================================
+
+    //=============================-Setters-==================================
+}
