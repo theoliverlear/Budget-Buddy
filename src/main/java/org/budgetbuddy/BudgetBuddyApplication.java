056838d68082fdc3e98fb2ec79bc83e31349fbba
theoliverlear <sigwartholiver2@gmail.com>
2024-03-03 19:33:47 -0600

    Add header comments to BudgetBuddyApplication.java

--- src/main/java/org/budgetbuddy/BudgetBuddyApplication.java
+++ src/main/java/org/budgetbuddy/BudgetBuddyApplication.java
@@ -1,10 +1,28 @@
 package org.budgetbuddy;
-
+//=================================-Imports-==================================
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BudgetBuddyApplication {
+    //============================-Variables-=================================
+
+    //===========================-Constructors-===============================
+
+    //=============================-Methods-==================================
+
+    //============================-Overrides-=================================
+
+    //------------------------------Equals------------------------------------
+
+    //------------------------------Hash-Code---------------------------------
+
+    //------------------------------To-String---------------------------------
+
+    //=============================-Getters-==================================
+
+    //=============================-Setters-==================================
+    //===============================-Main-===================================
     public static void main(String[] args) {
         SpringApplication.run(BudgetBuddyApplication.class, args);
     }
