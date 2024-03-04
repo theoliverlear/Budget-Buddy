ada28c12ae6f48f09a58516fc162f2cd238f3ae6
theoliverlear <sigwartholiver2@gmail.com>
2024-03-03 19:34:55 -0600

    Add header comments to BudgetBuddyController.java

--- src/main/java/org/budgetbuddy/controller/BudgetBuddyController.java
+++ src/main/java/org/budgetbuddy/controller/BudgetBuddyController.java
@@ -1,12 +1,33 @@
 package org.budgetbuddy.controller;
-
+//=================================-Imports-==================================
+import org.budgetbuddy.service.BudgetBuddyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BudgetBuddyController {
+    //============================-Variables-=================================
+    @Autowired
+    BudgetBuddyService budgetBuddyService;
+    //===========================-Constructors-===============================
+
+    //=============================-Methods-==================================
+
+    //-------------------------------Index------------------------------------
     @RequestMapping("/")
     public String index() {
         return "index";
     }
+    //============================-Overrides-=================================
+
+    //------------------------------Equals------------------------------------
+
+    //------------------------------Hash-Code---------------------------------
+
+    //------------------------------To-String---------------------------------
+
+    //=============================-Getters-==================================
+
+    //=============================-Setters-==================================
 }
