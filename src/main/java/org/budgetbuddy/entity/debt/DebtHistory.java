dffbaa10e1291648ce86e14a64a060e71fdf25a9
theoliverlear <sigwartholiver2@gmail.com>
2024-03-03 19:54:46 -0600

    Add DebtHistory.java to represent a given user's history of debt

---
+++ src/main/java/org/budgetbuddy/entity/debt/DebtHistory.java
@@ -0,0 +1,38 @@
+package org.budgetbuddy.entity.debt;
+
+import jakarta.persistence.*;
+import org.budgetbuddy.model.format.FormattedDate;
+import org.budgetbuddy.model.format.FormattedTime;
+
+import java.util.HashMap;
+
+//=================================-Imports-==================================
+@Entity
+public class DebtHistory {
+    //============================-Variables-=================================
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Transient // TODO: Create a converter for this, then remove annotation.
+    HashMap<Debt, FormattedDate> debtHistoryMap;
+    //===========================-Constructors-===============================
+    public DebtHistory() {
+        this.debtHistoryMap = new HashMap<>();
+    }
+    public DebtHistory(HashMap<Debt, FormattedDate> debtHistoryMap) {
+        this.debtHistoryMap = debtHistoryMap;
+    }
+    //=============================-Methods-==================================
+
+    //============================-Overrides-=================================
+
+    //------------------------------Equals------------------------------------
+
+    //------------------------------Hash-Code---------------------------------
+
+    //------------------------------To-String---------------------------------
+
+    //=============================-Getters-==================================
+
+    //=============================-Setters-==================================
+}
