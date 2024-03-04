991509ccd88131db9b0d6e1bc55278e04a9aa283
theoliverlear <sigwartholiver2@gmail.com>
2024-03-04 11:45:05 -0600

    Add header comments to SavingRepository.java

--- src/main/java/org/budgetbuddy/repository/SavingRepository.java
+++ src/main/java/org/budgetbuddy/repository/SavingRepository.java
@@ -1,8 +1,8 @@
 package org.budgetbuddy.repository;
-
+//=================================-Imports-==================================
 import org.budgetbuddy.entity.savings.Saving;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SavingRepository extends JpaRepository<Saving, Long> {
-
+    //=============================-Methods-==================================
 }
