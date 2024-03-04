9d1218007b161e1d5a79189681be26753b727f60
theoliverlear <sigwartholiver2@gmail.com>
2024-03-03 19:50:54 -0600

    Add CompoundInterest.java to represent compound interest accrued on Saving.java or Debt.java

---
+++ src/main/java/org/budgetbuddy/entity/interest/CompoundInterest.java
@@ -0,0 +1,54 @@
+package org.budgetbuddy.entity.interest;
+//=================================-Imports-==================================
+import jakarta.persistence.Entity;
+import org.budgetbuddy.entity.debt.Debt;
+import org.budgetbuddy.entity.savings.Saving;
+import org.budgetbuddy.entity.time.TimeInterval;
+
+@Entity
+public class CompoundInterest extends Interest {
+    //============================-Variables-=================================
+
+    //===========================-Constructors-===============================
+    public CompoundInterest() {
+        super();
+    }
+    public CompoundInterest(double rateAsDecimal) {
+        super(rateAsDecimal);
+    }
+    public CompoundInterest(double rateAsDecimal, TimeInterval chargeInterestInterval) {
+        super(rateAsDecimal, chargeInterestInterval);
+    }
+    //=============================-Methods-==================================
+
+    //============================-Overrides-=================================
+
+    //-------------------------Calculate-Interest-----------------------------
+    @Override
+    public double calculateInterest(double principal, TimeInterval timeInterval) {
+        // TODO: Implement cost of compound interest. Include interest on
+        //       interest on top of interest on principal.
+        return 0;
+    }
+    //------------------------Add-Interest-To-Debt----------------------------
+    @Override
+    public Debt addInterestToDebt(Debt debt) {
+        // TODO: Implement adding compound interest to debt.
+        return null;
+    }
+    //-----------------------Add-Interest-To-Saving---------------------------
+    @Override
+    public Saving addInterestToSaving(Saving saving) {
+        // TODO: Implement adding compound interest to saving.
+        return null;
+    }
+    //------------------------------Equals------------------------------------
+
+    //------------------------------Hash-Code---------------------------------
+
+    //------------------------------To-String---------------------------------
+
+    //=============================-Getters-==================================
+
+    //=============================-Setters-==================================
+}
