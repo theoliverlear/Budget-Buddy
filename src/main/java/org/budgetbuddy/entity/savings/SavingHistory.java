f4fd6aa49d7a16220f1b64c0d9aaa433c6285338
theoliverlear <sigwartholiver2@gmail.com>
2024-03-03 22:02:30 -0600

    Add SavingHistory.java to represent a user's saving history

---
+++ src/main/java/org/budgetbuddy/entity/savings/SavingHistory.java
@@ -0,0 +1,36 @@
+package org.budgetbuddy.entity.savings;
+//============================-Variables-=================================
+import jakarta.persistence.*;
+import org.budgetbuddy.model.format.FormattedDate;
+
+import java.util.HashMap;
+
+@Entity
+public class SavingHistory {
+    //============================-Variables-=================================
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    @Transient // TODO: Create a converter for this, then remove annotation.
+    HashMap<Saving, FormattedDate> savingHistoryMap;
+    //===========================-Constructors-===============================
+    public SavingHistory() {
+        this.savingHistoryMap = new HashMap<>();
+    }
+    public SavingHistory(HashMap<Saving, FormattedDate> savingHistoryMap) {
+        this.savingHistoryMap = savingHistoryMap;
+    }
+    //=============================-Methods-==================================
+
+    //============================-Overrides-=================================
+
+    //------------------------------Equals------------------------------------
+
+    //------------------------------Hash-Code---------------------------------
+
+    //------------------------------To-String---------------------------------
+
+    //=============================-Getters-==================================
+
+    //=============================-Setters-==================================
+}
