4546e809a3d2bccca61a30de4ef95ecb07ffaac5
theoliverlear <sigwartholiver2@gmail.com>
2024-03-08 09:21:23 -0600

    Add instance variables to BudgetBuddy.java

--- src/main/java/org/budgetbuddy/model/BudgetBuddy.java
+++ src/main/java/org/budgetbuddy/model/BudgetBuddy.java
@@ -1,10 +1,26 @@
 package org.budgetbuddy.model;
+
+import org.budgetbuddy.model.calculator.FinanceCalculator;
+import org.budgetbuddy.model.calculator.InterestCalculator;
+import org.budgetbuddy.model.graph.Graph;
+
 //=================================-Imports-==================================
 public class BudgetBuddy {
     //============================-Variables-=================================
-
+    Graph currentGraph;
+    FinanceCalculator financeCalculator;
+    InterestCalculator interestCalculator;
     //===========================-Constructors-===============================
-
+    public BudgetBuddy() {
+        currentGraph = null;
+        this.financeCalculator = new FinanceCalculator();
+        this.interestCalculator = new InterestCalculator();
+    }
+    public BudgetBuddy(Graph currentGraph, FinanceCalculator financeCalculator, InterestCalculator interestCalculator) {
+        this.currentGraph = currentGraph;
+        this.financeCalculator = financeCalculator;
+        this.interestCalculator = interestCalculator;
+    }
     //=============================-Methods-==================================
 
     //============================-Overrides-=================================
