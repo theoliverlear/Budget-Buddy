32c736920e1500449efb638916b7c0b8b894fcf6
theoliverlear <sigwartholiver2@gmail.com>
2024-03-09 18:54:22 -0600

    Add method to build a pie graph with a budget in GraphService.java

--- src/main/java/org/budgetbuddy/service/GraphService.java
+++ src/main/java/org/budgetbuddy/service/GraphService.java
@@ -1,6 +1,12 @@
 package org.budgetbuddy.service;
 //=================================-Imports-==================================
+import org.budgetbuddy.entity.budget.Budget;
+import org.budgetbuddy.entity.budget.BudgetItem;
+import org.budgetbuddy.model.graph.PieGraph;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Service
 public class GraphService {
@@ -9,7 +15,14 @@ public class GraphService {
     //===========================-Constructors-===============================
 
     //=============================-Methods-==================================
-
+    public PieGraph buildPieGraph(Budget userBudget) {
+        HashMap<String, Double> budgetData = new HashMap<>();
+        ArrayList<BudgetItem> budgetItems = userBudget.getBudgetItems();
+        for (BudgetItem budgetItem : budgetItems) {
+            budgetData.put(budgetItem.getName(), budgetItem.getAmount());
+        }
+        return new PieGraph(budgetData);
+    }
     //============================-Overrides-=================================
 
     //------------------------------Equals------------------------------------
