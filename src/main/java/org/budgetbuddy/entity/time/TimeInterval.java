3982363af1a9f8443880c024df28b4ebc46e1104
theoliverlear <sigwartholiver2@gmail.com>
2024-04-10 14:02:17 -0500

    Instantiate static constants and set identifying field to triggers per year in TimeInterval.java

--- src/main/java/org/budgetbuddy/entity/time/TimeInterval.java
+++ src/main/java/org/budgetbuddy/entity/time/TimeInterval.java
@@ -1,40 +1,106 @@
 package org.budgetbuddy.entity.time;
 //=================================-Imports-==================================
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class TimeInterval {
     //============================-Variables-=================================
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    double triggersPerYear;
+    String intervalType;
     //============================-Constants-=================================
-    // TODO: Add constants for the following time intervals.
-    public static final TimeInterval DAILY = null;
-    public static final TimeInterval WEEKLY = null;
-    public static final TimeInterval BIWEEKLY = null;
-    public static final TimeInterval MONTHLY = null;
-    public static final TimeInterval BIMONTHLY = null;
-    public static final TimeInterval QUARTERLY = null;
-    public static final TimeInterval SEMIANNUALLY = null;
-    public static final TimeInterval ANNUALLY = null;
-    public static final TimeInterval BIENNIALLY = null;
+    @Transient
+    public static final TimeInterval DAILY = new TimeInterval(365);
+    @Transient
+    public static final TimeInterval WEEKLY = new TimeInterval(52);
+    @Transient
+    public static final TimeInterval BIWEEKLY = new TimeInterval(26);
+    @Transient
+    public static final TimeInterval MONTHLY = new TimeInterval(12);
+    @Transient
+    public static final TimeInterval BIMONTHLY = new TimeInterval(6);
+    @Transient
+    public static final TimeInterval QUARTERLY = new TimeInterval(4);
+    @Transient
+    public static final TimeInterval SEMIANNUALLY = new TimeInterval(2);
+    @Transient
+    public static final TimeInterval ANNUALLY = new TimeInterval(1);
+    @Transient
+    public static final TimeInterval BIENNIALLY = new TimeInterval(0.5);
     //===========================-Constructors-===============================
-
+    public TimeInterval() {
+        this.triggersPerYear = 0;
+        this.intervalType = "Custom";
+    }
+    public TimeInterval(double triggersPerYear) {
+        this.triggersPerYear = triggersPerYear;
+        this.intervalType = this.determineIntervalType();
+    }
+    public TimeInterval(double triggersPerYear, String intervalType) {
+        this.triggersPerYear = triggersPerYear;
+        this.intervalType = intervalType;
+    }
     //=============================-Methods-==================================
 
+    //----------------------Determine-Interval-Type---------------------------
+    public String determineIntervalType() {
+        // NOTE: Switch statements do not work with double values. A long
+        //       if/else chain is easiest alternative.
+        if (this.triggersPerYear == 365) {
+            return "Daily";
+        } else if (this.triggersPerYear == 52) {
+            return "Weekly";
+        } else if (this.triggersPerYear == 26) {
+            return "Biweekly";
+        } else if (this.triggersPerYear == 12) {
+            return "Monthly";
+        } else if (this.triggersPerYear == 6) {
+            return "Bimonthly";
+        } else if (this.triggersPerYear == 4) {
+            return "Quarterly";
+        } else if (this.triggersPerYear == 2) {
+            return "Semiannually";
+        } else if (this.triggersPerYear == 1) {
+            return "Annually";
+        } else if (this.triggersPerYear == 0.5) {
+            return "Biennially";
+        } else {
+            return "Custom";
+        }
+    }
     //============================-Overrides-=================================
 
     //------------------------------Equals------------------------------------
-
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof TimeInterval timeInterval) {
+            return this.triggersPerYear == timeInterval.triggersPerYear;
+        }
+        return false;
+    }
     //------------------------------Hash-Code---------------------------------
-
+    @Override
+    public int hashCode() {
+        return Double.hashCode(this.triggersPerYear);
+    }
     //------------------------------To-String---------------------------------
-
+    @Override
+    public String toString() {
+        return "Occurs %f times per year".formatted(this.triggersPerYear);
+    }
     //=============================-Getters-==================================
-
+    public double getTriggersPerYear() {
+        return this.triggersPerYear;
+    }
+    public String getIntervalType() {
+        return this.intervalType;
+    }
     //=============================-Setters-==================================
+    public void setTriggersPerYear(double triggersPerYear) {
+        this.triggersPerYear = triggersPerYear;
+    }
+    public void setIntervalType(String intervalType) {
+        this.intervalType = intervalType;
+    }
 }
