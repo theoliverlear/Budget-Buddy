333f59cbce8e0af343e31355a6bdb9b82f0ae994
theoliverlear <sigwartholiver2@gmail.com>
2024-04-10 14:42:15 -0500

    Add getters, setters, and override methods in Expense.java

--- src/main/java/org/budgetbuddy/entity/expense/Expense.java
+++ src/main/java/org/budgetbuddy/entity/expense/Expense.java
@@ -36,12 +36,48 @@ public class Expense {
     //============================-Overrides-=================================
 
     //------------------------------Equals------------------------------------
-
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof Expense expense) {
+            return this.id.equals(expense.id);
+        }
+        return false;
+    }
     //------------------------------Hash-Code---------------------------------
-
+    @Override
+    public int hashCode() {
+        return this.id.hashCode();
+    }
     //------------------------------To-String---------------------------------
-
+    @Override
+    public String toString() {
+        return "%s: $%.2f - %s".formatted(this.name, this.amount, this.category);
+    }
     //=============================-Getters-==================================
-
+    public Long getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public double getAmount() {
+        return this.amount;
+    }
+    public Category getCategory() {
+        return this.category;
+    }
     //=============================-Setters-==================================
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
