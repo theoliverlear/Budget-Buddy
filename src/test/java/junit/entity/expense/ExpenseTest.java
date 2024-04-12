e18064c3fb63bb54cbde2dae83a098a6f8d6c1a5
gregdemko <gregory.demko@my.uwrf.edu>
2024-04-11 20:12:05 -0500

    Add test object and headers to ExpenseTest.java

--- src/test/java/junit/entity/expense/ExpenseTest.java
+++ src/test/java/junit/entity/expense/ExpenseTest.java
@@ -1,4 +1,17 @@
 package junit.entity.expense;
 
+import org.budgetbuddy.entity.expense.Expense;
+
 public class ExpenseTest {
+    //============================-Variables-=================================
+    Expense testExpense = new Expense("Lunch", 5);
+
+
+    //----------------------------Test-Instantiation--------------------------------
+
+    //----------------------------Test-Methods--------------------------------
+
+    //----------------------------Test-Getters--------------------------------
+
+    //----------------------------Test-Setters--------------------------------
 }
