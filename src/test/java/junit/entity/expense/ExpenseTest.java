4d33a957da49ecc3026b4b953ac6e5592308b278
gregdemko <gregory.demko@my.uwrf.edu>
2024-04-11 20:20:05 -0500

    Add test instantiation and test getters for the two instance variables in ExpenseTest.java

--- src/test/java/junit/entity/expense/ExpenseTest.java
+++ src/test/java/junit/entity/expense/ExpenseTest.java
@@ -1,6 +1,8 @@
 package junit.entity.expense;
 
 import org.budgetbuddy.entity.expense.Expense;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ExpenseTest {
     //============================-Variables-=================================
@@ -8,10 +10,20 @@ public class ExpenseTest {
 
 
     //----------------------------Test-Instantiation--------------------------------
+    @Test
+    public void testExpenseNotNull(){
+        assertNotNull(testExpense);
+    }
 
     //----------------------------Test-Methods--------------------------------
 
     //----------------------------Test-Getters--------------------------------
+    public void testGetName(){
+        assertEquals("Lunch", this.testExpense.getName());
+    }
+    public void testGetAmount(){
+        assertEquals(5, this.testExpense.getAmount());
+    }
 
     //----------------------------Test-Setters--------------------------------
 }
