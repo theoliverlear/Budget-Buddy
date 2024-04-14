936d2daaea180678d48c72d1ba1ab6651b4884d0
gregdemko <gregory.demko@my.uwrf.edu>
2024-04-13 20:19:52 -0500

    Add headers and test objects to RevenueTest.java

--- src/test/java/junit/entity/revenue/RevenueTest.java
+++ src/test/java/junit/entity/revenue/RevenueTest.java
@@ -1,4 +1,23 @@
 package junit.entity.revenue;
 
+import org.budgetbuddy.entity.revenue.Revenue;
+import org.budgetbuddy.entity.revenue.Revenues;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RevenueTest {
+    //============================-Variables-=================================
+    Revenues testRevenues = new Revenues();
+    Revenue testRevenue = new Revenue("Paycheck", 2000);
+
+
+    //----------------------------Test-Instantiation--------------------------------
+
+    //----------------------------Test-Methods--------------------------------
+
+    //----------------------------Test-Getters--------------------------------
+
+    //----------------------------Test-Setters--------------------------------
+
+
 }
