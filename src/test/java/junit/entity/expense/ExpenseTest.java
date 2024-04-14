83b44eeb719faa859c7be618bfaba518c3fd1f8a
gregdemko <gregory.demko@my.uwrf.edu>
2024-04-13 19:41:41 -0500

    Add getter and setter tests for Category in ExpenseTest.java

--- src/test/java/junit/entity/expense/ExpenseTest.java
+++ src/test/java/junit/entity/expense/ExpenseTest.java
@@ -1,12 +1,13 @@
 package junit.entity.expense;
 
+import org.budgetbuddy.entity.category.Category;
 import org.budgetbuddy.entity.expense.Expense;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ExpenseTest {
     //============================-Variables-=================================
-    Expense testExpense = new Expense("Lunch", 5);
+    Expense testExpense = new Expense("Lunch", 5, Category.FOOD);
 
 
     //----------------------------Test-Instantiation--------------------------------
@@ -18,20 +19,33 @@ public class ExpenseTest {
     //----------------------------Test-Methods--------------------------------
 
     //----------------------------Test-Getters--------------------------------
+    @Test
     public void testGetName(){
         assertEquals("Lunch", this.testExpense.getName());
     }
+    @Test
     public void testGetAmount(){
         assertEquals(5, this.testExpense.getAmount());
     }
 
+    @Test
+    public void testGetCategory(){
+        assertEquals(Category.FOOD, this.testExpense.getCategory());
+    }
     //----------------------------Test-Setters--------------------------------
+    @Test
     public void testSetName(){
         this.testExpense.setName("T-Shirt");
         assertEquals("T-Shirt", this.testExpense.getName());
     }
+    @Test
     public void testSetAmount(){
         this.testExpense.setAmount(20);
         assertEquals(20, this.testExpense.getAmount());
+    }
+    @Test
+    public void testSetExpense() {
+        this.testExpense.setCategory(Category.ENTERTAINMENT);
+        assertEquals(Category.ENTERTAINMENT, this.testExpense.getCategory());
     }
 }
