09af5e0b8543a7dd4c5d531d18c653b447dfa236
gregdemko <gregory.demko@my.uwrf.edu>
2024-04-13 20:28:29 -0500

    Add test getters and setters to RevenueTest.java

--- src/test/java/junit/entity/revenue/RevenueTest.java
+++ src/test/java/junit/entity/revenue/RevenueTest.java
@@ -1,5 +1,6 @@
 package junit.entity.revenue;
 
+import org.budgetbuddy.entity.category.Category;
 import org.budgetbuddy.entity.revenue.Revenue;
 import org.budgetbuddy.entity.revenue.Revenues;
 import org.junit.jupiter.api.Test;
@@ -12,12 +13,43 @@ public class RevenueTest {
 
 
     //----------------------------Test-Instantiation--------------------------------
+    @Test
+    public void testRevenuesNotNull(){
+        assertNotNull(this.testRevenues);
+    }
+    @Test
+    public void testRevenueNotNull(){
+        assertNotNull(this.testRevenue);
+    }
 
     //----------------------------Test-Methods--------------------------------
 
     //----------------------------Test-Getters--------------------------------
+    @Test
+    public void testGetName(){
+        assertEquals("Paycheck", this.testRevenue.getName());
+    }
+    @Test
+    public void testGetAmount(){
+        assertEquals(2000, this.testRevenue.getAmount());
+    }
 
     //----------------------------Test-Setters--------------------------------
+    @Test
+    public void testSetName(){
+        this.testRevenue.setName("Stock Dividend");
+        assertEquals("Stock Dividend", this.testRevenue.getName());
+    }
+    @Test
+    public void testSetAmount(){
+        this.testRevenue.setAmount(10);
+        assertEquals(10, this.testRevenue.getAmount());
+    }
+    @Test
+    public void testSetId(){
+        this.testRevenue.setId(100123L);
+        assertEquals(100123L, this.testRevenue.getId());
+    }
 
 
 }
