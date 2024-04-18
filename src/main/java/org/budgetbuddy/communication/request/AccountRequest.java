acfc5e7561570eead1bfb4d2128dadb2944cb17a
theoliverlear <sigwartholiver2@gmail.com>
2024-04-17 20:28:36 -0500

    Rename LoginRequest.java to AccountRequest.java and add fields

--- src/main/java/org/budgetbuddy/communication/request/AccountRequest.java
+++ src/main/java/org/budgetbuddy/communication/request/AccountRequest.java
@@ -1,10 +1,18 @@
 package org.budgetbuddy.communication.request;
 //=================================-Imports-==================================
-public class LoginRequest {
+public class AccountRequest {
     //============================-Variables-=================================
-
+    String username;
+    String password;
     //===========================-Constructors-===============================
-
+    public AccountRequest() {
+        this.username = "";
+        this.password = "";
+    }
+    public AccountRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
     //=============================-Methods-==================================
 
     //============================-Overrides-=================================
@@ -16,7 +24,17 @@ public class LoginRequest {
     //------------------------------To-String---------------------------------
 
     //=============================-Getters-==================================
-
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
     //=============================-Setters-==================================
-
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
