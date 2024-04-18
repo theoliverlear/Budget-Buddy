49f4a5639e393173041341e0fe65429972e6abed
theoliverlear <sigwartholiver2@gmail.com>
2024-04-17 20:40:43 -0500

    Add DebtHistoryConverter.java to convert DebtHistory.java files for the database

---
+++ src/main/java/org/budgetbuddy/convert/entity/debt/DebtHistoryConverter.java
@@ -0,0 +1,39 @@
+package org.budgetbuddy.convert.entity.debt;
+//=================================-Imports-==================================
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import org.budgetbuddy.entity.holding.debt.DebtHistory;
+
+@Converter(autoApply = true)
+public class DebtHistoryConverter implements AttributeConverter<DebtHistory, String> {
+    //============================-Variables-=================================
+    ObjectMapper objectMapper;
+    //===========================-Constructors-===============================
+    public DebtHistoryConverter() {
+        this.objectMapper = new ObjectMapper();
+    }
+    //============================-Overrides-=================================
+
+    //---------------------Convert-To-Database-Column-------------------------
+    @Override
+    public String convertToDatabaseColumn(DebtHistory debtHistory) {
+        try {
+            return this.objectMapper.writeValueAsString(debtHistory);
+        } catch (JsonProcessingException ex) {
+            final String EXCEPTION_MESSAGE = "Error converting debt history to JSON.";
+            throw new RuntimeException(EXCEPTION_MESSAGE, ex);
+        }
+    }
+    //--------------------Convert-From-Database-Column------------------------
+    @Override
+    public DebtHistory convertToEntityAttribute(String debtHistoryJson) {
+        try {
+            return this.objectMapper.readValue(debtHistoryJson, DebtHistory.class);
+        } catch (JsonProcessingException ex) {
+            final String EXCEPTION_MESSAGE = "Error converting JSON to debt history.";
+            throw new RuntimeException(EXCEPTION_MESSAGE, ex);
+        }
+    }
+}
