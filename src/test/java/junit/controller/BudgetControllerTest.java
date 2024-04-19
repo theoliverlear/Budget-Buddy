df062dbe078d1587b5be85e8228558180eceef5f
gregdemko <gregory.demko@my.uwrf.edu>
2024-04-19 15:46:36 -0500

    Add instantiation test for testBudgetController in BudgetControllerTest.java

--- src/test/java/junit/controller/BudgetControllerTest.java
+++ src/test/java/junit/controller/BudgetControllerTest.java
@@ -1,4 +1,16 @@
 package junit.controller;
 
+import org.budgetbuddy.controller.BudgetController;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BudgetControllerTest {
+    //============================-Variables-=================================
+    BudgetController testBudgetController = new BudgetController();
+
+    //----------------------------Test-Instantiation--------------------------------
+    @Test
+    public void testBudgetControllerNotNull(){
+        assertNotNull(testBudgetController);
+    }
 }
