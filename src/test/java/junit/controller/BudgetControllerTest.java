fcfaeeff74a1a4ed8a1a0b2f4c258a10d86a5147
theoliverlear <sigwartholiver2@gmail.com>
2024-04-20 12:50:41 -0500

    Refactor tests, add payloads, and comment headers BudgetControllerTest.java

--- src/test/java/junit/controller/BudgetControllerTest.java
+++ src/test/java/junit/controller/BudgetControllerTest.java
@@ -1,53 +1,58 @@
 package junit.controller;
-
+//=================================-Imports-==================================
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.budgetbuddy.communication.request.BudgetItemRequest;
 import org.budgetbuddy.controller.BudgetController;
 import org.junit.jupiter.api.Test;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = BudgetController.class)
+@AutoConfigureMockMvc
+@WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
+@ActiveProfiles("test")
 public class BudgetControllerTest {
     //============================-Variables-=================================
+    @Autowired
+    MockMvc mockMvc;
+    ObjectMapper objectMapper = new ObjectMapper();
+    //==============================-Tests-===================================
 
-    BudgetController testBudgetController = new BudgetController();
-
-    private MockMvc mockMvc;
-    @BeforeEach
-    public void setUp() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(testBudgetController).build();
-    }
-
-    //----------------------------Test-Instantiation--------------------------------
+    //------------------------Controller-Not-Null-----------------------------
     @Test
     public void testBudgetControllerNotNull(){
-        assertNotNull(testBudgetController);
+        assertNotNull(this.mockMvc);
     }
-
-    //----------------------------Test-Methods--------------------------------
+    //------------------------Test-Budget-Homepage----------------------------
     @Test
     public void testBudget() throws Exception{
-        mockMvc.perform(post("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("budget"));
-
+        this.mockMvc.perform(get("/budget/")
+                        .with(csrf()))
+                        .andExpect(status().isOk());
     }
+    //------------------------Test-Add-Budget-Item----------------------------
     @Test
-    public void testAdd() throws Exception{
-        mockMvc.perform(get("/add")
-                .param("food", "50"))
+    public void testAddBudgetItem() throws Exception {
+        BudgetItemRequest budgetItemRequest = new BudgetItemRequest("food", 50, "grocery");
+        this.mockMvc.perform(post("/budget/add")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(budgetItemRequest)))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Sucsess"));
-
     }
-
 }
