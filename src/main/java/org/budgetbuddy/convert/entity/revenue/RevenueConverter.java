611e21142299fc671b60ceb8c685bb96e8ab4062
theoliverlear <sigwartholiver2@gmail.com>
2024-04-22 20:55:16 -0500

    Add RevenueConverter.java to handle converting Revenue to and from JSON

--- src/main/java/org/budgetbuddy/convert/entity/revenue/RevenueConverter.java
+++ src/main/java/org/budgetbuddy/convert/entity/revenue/RevenueConverter.java
@@ -1,38 +1,38 @@
-package org.budgetbuddy.entity.user;
+package org.budgetbuddy.convert.entity.revenue;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
-import org.budgetbuddy.entity.finance.Finance;
+import org.budgetbuddy.entity.revenue.Revenue;
 
 @Converter(autoApply = true)
-public class FinanceConverter implements AttributeConverter<Finance, String> {
+public class RevenueConverter implements AttributeConverter<Revenue, String> {
     //============================-Variables-=================================
     ObjectMapper objectMapper;
     //===========================-Constructors-===============================
-    public FinanceConverter() {
+    public RevenueConverter() {
         this.objectMapper = new ObjectMapper();
     }
     //============================-Overrides-=================================
 
     //---------------------Convert-To-Database-Column-------------------------
     @Override
-    public String convertToDatabaseColumn(Finance finance) {
+    public String convertToDatabaseColumn(Revenue revenue) {
         try {
-            return this.objectMapper.writeValueAsString(finance);
+            return this.objectMapper.writeValueAsString(revenue);
         } catch (JsonProcessingException ex) {
-            final String EXCEPTION_MESSAGE = "Error converting finance to JSON.";
+            final String EXCEPTION_MESSAGE = "Error converting revenue to JSON.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
     }
     //--------------------Convert-From-Database-Column------------------------
     @Override
-    public Finance convertToEntityAttribute(String financeJson) {
+    public Revenue convertToEntityAttribute(String revenueJson) {
         try {
-            return this.objectMapper.readValue(financeJson, Finance.class);
+            return this.objectMapper.readValue(revenueJson, Revenue.class);
         } catch (JsonProcessingException ex) {
-            final String EXCEPTION_MESSAGE = "Error converting JSON to finance.";
+            final String EXCEPTION_MESSAGE = "Error converting JSON to revenue.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
     }
