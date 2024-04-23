41f2173b02e31c64163dca9a630d678b774d010c
theoliverlear <sigwartholiver2@gmail.com>
2024-04-22 21:04:53 -0500

    Add a query to retrieve current budget by user ID in UserRepository.java

--- src/main/java/org/budgetbuddy/repository/UserRepository.java
+++ src/main/java/org/budgetbuddy/repository/UserRepository.java
@@ -3,11 +3,13 @@ package org.budgetbuddy.repository;
 import org.budgetbuddy.entity.budget.Budget;
 import org.budgetbuddy.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     //=============================-Methods-==================================
     User findByUsername(String username);
     User getUserById(Long id);
+    @Query("SELECT u.currentBudget FROM User u WHERE u.id = :id")
     Budget getBudgetById(Long id);
     boolean existsByUsername(String username);
 }
