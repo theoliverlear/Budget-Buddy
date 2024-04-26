7b28473ce2c49faa686177067e4229d66ccfccd7
gregdemko <gregory.demko@my.uwrf.edu>
2024-04-26 18:42:04 -0500

    Add testRequestSpeed to RequestTest.java. Error needs to be fixed

--- src/test/java/junit/communication/request/RequestTest.java
+++ src/test/java/junit/communication/request/RequestTest.java
@@ -1,24 +1,26 @@
 package junit.communication.request;
 //=================================-Imports-==================================
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.budgetbuddy.communication.request.BudgetItemRequest;
+import org.budgetbuddy.BudgetBuddyApplication;
+import org.budgetbuddy.service.BudgetService;
 import org.budgetbuddy.controller.BudgetController;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = BudgetController.class)
@@ -28,13 +30,26 @@ import org.springframework.test.web.servlet.MockMvc;
 public class RequestTest {
     //============================-Variables-=================================
     @Autowired
-    MockMvc mockMvc;
-    ObjectMapper objectMapper = new ObjectMapper();
-    //==============================-Tests-===================================
+    private MockMvc mockMvc;
 
-    //------------------------Test-Budget-Homepage----------------------------
-    @Test
-    public void requestSpeedTest(){
+    private static final int NUMBER_OF_REQUESTS = 100;
+    private static long startTime;
 
+    @BeforeAll
+    public static void startTime() {
+        startTime = System.currentTimeMillis();
+    }
+
+    @RepeatedTest(NUMBER_OF_REQUESTS)
+    public void testRequest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/budget/"))
+                .andExpect(status().isOk());
+    }
+
+    @AfterAll
+    public static void testRequestSpeed() {
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        assert duration <= 1000 : "Took longer than 1 second to process " + NUMBER_OF_REQUESTS + " requests";
     }
 }
