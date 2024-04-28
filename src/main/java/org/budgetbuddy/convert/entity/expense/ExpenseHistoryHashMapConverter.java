b4ae8d80b78a1b3ae4f585f250d5a3ec2f0dd4a8
theoliverlear <sigwartholiver2@gmail.com>
2024-04-28 14:13:30 -0500

    Add contextualizing comments to ExpenseHistoryHashMapConverter.java

--- src/main/java/org/budgetbuddy/convert/entity/expense/ExpenseHistoryHashMapConverter.java
+++ src/main/java/org/budgetbuddy/convert/entity/expense/ExpenseHistoryHashMapConverter.java
@@ -1,5 +1,5 @@
 package org.budgetbuddy.convert.entity.expense;
-
+//=================================-Imports-==================================
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,8 +23,11 @@ public class ExpenseHistoryHashMapConverter implements AttributeConverter<HashMa
     @Override
     public String convertToDatabaseColumn(HashMap<Expense, LocalDate> expenseHistory) {
         try {
+            // Convert the expense history map to a JSON string.
             return this.objectMapper.writeValueAsString(expenseHistory);
         } catch (JsonProcessingException ex) {
+            // If an error occurs, throw a runtime exception to stop the
+            // program from using invalid data.
             final String EXCEPTION_MESSAGE = "Error converting expense history to JSON.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
@@ -33,11 +36,16 @@ public class ExpenseHistoryHashMapConverter implements AttributeConverter<HashMa
     @Override
     public HashMap<Expense, LocalDate> convertToEntityAttribute(String expenseHistoryJson) {
         try {
+            // If the JSON string is null, return an empty map to indicate an
+            // empty expense history.
             if (expenseHistoryJson == null) {
                 return new HashMap<>();
             }
+            // Convert the JSON string to an expense history map.
             return this.objectMapper.readValue(expenseHistoryJson, new TypeReference<>() {});
         } catch (JsonProcessingException ex) {
+            // If an error occurs, throw a runtime exception to stop the
+            // program from using invalid data.
             final String EXCEPTION_MESSAGE = "Error converting JSON to expense history.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
