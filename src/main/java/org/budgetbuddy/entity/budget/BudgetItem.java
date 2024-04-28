5b0fa3eebe7be4699b6dc85a8e91c16e69db5665
theoliverlear <sigwartholiver2@gmail.com>
2024-04-28 16:36:07 -0500

    Add contextualizing comments to BudgetItem.java

--- src/main/java/org/budgetbuddy/entity/budget/BudgetItem.java
+++ src/main/java/org/budgetbuddy/entity/budget/BudgetItem.java
@@ -1,10 +1,9 @@
 package org.budgetbuddy.entity.budget;
-
+//=================================-Imports-==================================
 import jakarta.persistence.*;
 import org.budgetbuddy.convert.entity.category.CategoryConverter;
 import org.budgetbuddy.entity.category.Category;
 
-//=================================-Imports-==================================
 @Entity
 public class BudgetItem {
     //============================-Variables-=================================
@@ -36,26 +35,42 @@ public class BudgetItem {
     //------------------------------Equals------------------------------------
     @Override
     public boolean equals(Object obj) {
+        // Check if the object references are the same. If they are, return
+        // true.
         if (this == obj) return true;
+        // Check if the object is an instance of BudgetItem. If it is, cast it
+        // to a BudgetItem object.
         if (obj instanceof BudgetItem budgetItem) {
+            // Check if the fields of the BudgetItem objects are equal.
             boolean sameName = this.name.equals(budgetItem.name);
             boolean sameAmount = this.amount == budgetItem.amount;
             boolean sameCategory = this.category.equals(budgetItem.category);
+            // If the BudgetItem id is not null, an equality check can be
+            // performed using that field.
             if (this.id != null) {
+                // Return whether all fields are equal.
                 boolean sameId = this.id.equals(budgetItem.id);
                 return sameId && sameName && sameAmount && sameCategory;
             } else {
+                // If the BudgetItem id is null, return whether all fields are
+                // equal except for the id field.
                 return sameName && sameAmount && sameCategory;
             }
         }
+        // If we have reached this point, the objects are not instances of the
+        // same class and are not equal, so we return false.
         return false;
     }
     //------------------------------Hash-Code---------------------------------
     @Override
     public int hashCode() {
+        // If the BudgetItem id is not null, return the hashcode of the id
+        // field.
         if (this.id != null) {
             return this.id.hashCode();
         } else {
+            // If the BudgetItem id is null, return the hashcode of the name,
+            // amount, and category fields combined.
             int combinedHashCode = this.name.hashCode();
             combinedHashCode += Double.hashCode(this.amount);
             combinedHashCode += this.category.hashCode();
