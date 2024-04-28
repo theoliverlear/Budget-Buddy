f2b9ee0d4bb451f6c28f6070038e34f2215ed3f6
theoliverlear <sigwartholiver2@gmail.com>
2024-04-28 16:35:15 -0500

    Add contextualizing comments to Budget.java

--- src/main/java/org/budgetbuddy/entity/budget/Budget.java
+++ src/main/java/org/budgetbuddy/entity/budget/Budget.java
@@ -26,62 +26,99 @@ public class Budget {
     //=============================-Methods-==================================
 
     //--------------------------Add-Budget-Items------------------------------
-    public void addBudgetItem(BudgetItem budgetItem) { //adding Budget Item method
+    public void addBudgetItem(BudgetItem budgetItem) {
+        // Add the budget item to the list of BudgetItems.
         this.budgetItems.add(budgetItem);
     }
-    public boolean removeBudgetItem(BudgetItem budgetItem) {//removing Budget Item
+    //-------------------------Remove-Budget-Item-----------------------------
+    public boolean removeBudgetItem(BudgetItem budgetItem) {
+        // Remove the budget item from the list of BudgetItems. Return whether
+        // the budget item was removed successfully.
         return this.budgetItems.remove(budgetItem);
     }
+    //-------------------------Update-Budget-Item-----------------------------
     public boolean updateBudgetItem(BudgetItem updatedBudgetItem) {
+        // Loop through the list of BudgetItems to find the one that matches
+        // the name of the item to update.
         for (BudgetItem budgetItem : this.budgetItems) {
             if (budgetItem.getName().equals(updatedBudgetItem.getName())) {
+                // If the item is found, update the item fields and return
+                // true to indicate a successful update.
                 budgetItem.setAmount(updatedBudgetItem.getAmount());
                 budgetItem.setCategory(updatedBudgetItem.getCategory());
                 return true;
             }
         }
+        // If the item is not found, return false to indicate an unsuccessful
+        // update.
         return false;
     }
+    //-------------------------Update-Budget-Item-----------------------------
     public boolean updateBudgetItem(BudgetItem updatedBudgetItem, String newBudgetItemName) {
+        // Loop through the list of BudgetItems to find the one that matches
+        // the name of the item to update.
         for (BudgetItem budgetItem : this.budgetItems) {
             if (budgetItem.getName().equals(updatedBudgetItem.getName())) {
+                // If the item is found, update the item fields and return
+                // true to indicate a successful update.
                 budgetItem.setName(newBudgetItemName);
                 budgetItem.setAmount(updatedBudgetItem.getAmount());
                 budgetItem.setCategory(updatedBudgetItem.getCategory());
                 return true;
             }
         }
+        // If the item is not found, return false to indicate an unsuccessful
+        // update.
         return false;
     }
-    public BudgetItem getBudgetItemByTitle(String budgetItemName){//gets item by title
-        BudgetItem budgetItemFound=null;
-        for(BudgetItem budgetItem : this.getBudgetItems()){
-            if(budgetItem.getName().equals(budgetItemName)){
+    //----------------------Get-Budget-Item-By-Title--------------------------
+    public BudgetItem getBudgetItemByTitle(String budgetItemName) {
+        // Loop through the list of BudgetItems to find the one that matches
+        // the name of the item to get.
+        BudgetItem budgetItemFound = null;
+        for (BudgetItem budgetItem : this.getBudgetItems()) {
+            // If the item is found, return the item.
+            if (budgetItem.getName().equals(budgetItemName)) {
                 return budgetItem;
             }
         }
+        // If the item is not found, return null.
          return null;
     }
     //============================-Overrides-=================================
 
     //------------------------------Equals------------------------------------
     @Override
-    public boolean equals(Object obj){
+    public boolean equals(Object obj) {
+        // Check if the object references are the same. If they are, return
+        // true.
         if (this == obj) return true;
+        // Check if the object is an instance of the Budget class. If it is,
+        // cast it to a Budget object.
         if (obj instanceof Budget budget) {
+            // Check if the budget items are the same. If they are, return
+            // true.
             boolean sameBudgetItems = this.budgetItems.equals(budget.getBudgetItems());
+            // If id is not null, an equality check can be performed using
+            // that field.
             if (this.id != null) {
+                // Return whether all fields are equal.
                 boolean sameId = this.id.equals(budget.getId());
                 return sameId && sameBudgetItems;
             } else {
+                // If id is null, return whether all fields are equal except
+                // for the id field.
                 return sameBudgetItems;
             }
         }
+        // If we have reached this point, the objects not instances of the
+        // same class and are not equal, so we return false.
         return false;
     }
     //------------------------------Hash-Code---------------------------------
     @Override
     public int hashCode() {
+        // Return the hashcode of the id field.
         return this.id.hashCode();
     }
     //------------------------------To-String---------------------------------
