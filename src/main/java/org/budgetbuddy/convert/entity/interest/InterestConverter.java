16e69963b87a274374716432820015465531c081
theoliverlear <sigwartholiver2@gmail.com>
2024-04-28 15:32:13 -0500

    Add contextualizing comments to InterestConverter.java

--- src/main/java/org/budgetbuddy/convert/entity/interest/InterestConverter.java
+++ src/main/java/org/budgetbuddy/convert/entity/interest/InterestConverter.java
@@ -23,8 +23,11 @@ public class InterestConverter implements AttributeConverter<Interest, String> {
     @Override
     public String convertToDatabaseColumn(Interest interest) {
         try {
+            // Convert the Interest to a JSON string.
             return this.objectMapper.writeValueAsString(interest);
         } catch (JsonProcessingException ex) {
+            // If an error occurs, throw a runtime exception to stop the
+            // program from using invalid data.
             final String EXCEPTION_MESSAGE = "Error converting interest to JSON.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
@@ -33,19 +36,31 @@ public class InterestConverter implements AttributeConverter<Interest, String> {
     @Override
     public Interest convertToEntityAttribute(String interestJson) {
         try {
+            // Convert the JSON string to an Interest object. The JSON string
+            // must contain a field called "interest_type" that specifies the
+            // type of interest.
             JsonNode jsonNode = this.objectMapper.readTree(interestJson);
             if (jsonNode.has("interest_type")) {
                 String type = jsonNode.get("interest_type").asText();
+                // If the interest type is "simple", convert the JSON string
+                // to a SimpleInterest object.
                 if (type.equals("simple")) {
                     return this.objectMapper.readValue(interestJson, SimpleInterest.class);
                 } else if (type.equals("compound")) {
+                    // If the interest type is "compound", convert the JSON
+                    // string to a CompoundInterest object.
                     return this.objectMapper.readValue(interestJson, CompoundInterest.class);
                 }
             }
         } catch (JsonProcessingException ex) {
+            // If an error occurs, throw a runtime exception to stop the
+            // program from using invalid data.
             final String EXCEPTION_MESSAGE = "Error converting JSON to interest.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
+        // If we have reached this point, the interest type was not specified
+        // in the JSON string. A runtime exception is thrown to stop the
+        // program from using invalid data.
         throw new RuntimeException("Interest type not specified in JSON.");
     }
 }
