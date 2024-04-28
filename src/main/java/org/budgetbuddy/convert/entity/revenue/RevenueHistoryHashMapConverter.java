954bcecc965331d3b26d5c45abb394488ef2fb28
theoliverlear <sigwartholiver2@gmail.com>
2024-04-28 15:36:08 -0500

    Add contextualizing comments in RevenueHistoryHashMapConverter.java

--- src/main/java/org/budgetbuddy/convert/entity/revenue/RevenueHistoryHashMapConverter.java
+++ src/main/java/org/budgetbuddy/convert/entity/revenue/RevenueHistoryHashMapConverter.java
@@ -25,8 +25,11 @@ public class RevenueHistoryHashMapConverter implements AttributeConverter<HashMa
     @Override
     public String convertToDatabaseColumn(HashMap<Revenue, LocalDateTime> revenueHistory) {
         try {
+            // Convert the revenue history to a JSON string.
             return this.objectMapper.writeValueAsString(revenueHistory);
         } catch (JsonProcessingException ex) {
+            // If an error occurs, throw a runtime exception to stop the
+            // program from using invalid data.
             final String EXCEPTION_MESSAGE = "Error converting revenue history to JSON.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
@@ -35,11 +38,16 @@ public class RevenueHistoryHashMapConverter implements AttributeConverter<HashMa
     @Override
     public HashMap<Revenue, LocalDateTime> convertToEntityAttribute(String revenueHistoryJson) {
         try {
+            // If the JSON string is null, return an empty map to indicate an
+            // empty revenue history.
             if (revenueHistoryJson == null) {
                 return new HashMap<>();
             }
+            // Convert the JSON string to a revenue history map.
             return this.objectMapper.readValue(revenueHistoryJson, new TypeReference<>() {});
         } catch (JsonProcessingException ex) {
+            // If an error occurs, throw a runtime exception to stop the
+            // program from using invalid data.
             final String EXCEPTION_MESSAGE = "Error converting JSON to revenue history map.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
