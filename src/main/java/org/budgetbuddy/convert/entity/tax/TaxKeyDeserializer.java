b7b2f3106b80c01155d6ebb31c37271696da280c
theoliverlear <sigwartholiver2@gmail.com>
2024-04-28 15:44:01 -0500

    Add contextualizing comments to TaxKeyDeserializer.java

--- src/main/java/org/budgetbuddy/convert/entity/tax/TaxKeyDeserializer.java
+++ src/main/java/org/budgetbuddy/convert/entity/tax/TaxKeyDeserializer.java
@@ -1,5 +1,5 @@
 package org.budgetbuddy.convert.entity.tax;
-
+//=================================-Imports-==================================
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.KeyDeserializer;
 import org.budgetbuddy.entity.tax.Tax;
@@ -16,6 +16,7 @@ public class TaxKeyDeserializer extends KeyDeserializer {
     //--------------------------Deserialize-Key-------------------------------
     @Override
     public Tax deserializeKey(String key, DeserializationContext context) {
+        // Use a TaxConverter to convert the key to a Tax object.
         return this.taxConverter.convertToEntityAttribute(key);
     }
 }
