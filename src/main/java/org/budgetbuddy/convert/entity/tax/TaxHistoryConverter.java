b7ade874cbe30dc759184cecc90228048eb47fa9
theoliverlear <sigwartholiver2@gmail.com>
2024-04-28 15:42:32 -0500

    Add contextualizing comments to TaxHistoryConverter.java

--- src/main/java/org/budgetbuddy/convert/entity/tax/TaxHistoryConverter.java
+++ src/main/java/org/budgetbuddy/convert/entity/tax/TaxHistoryConverter.java
@@ -21,8 +21,11 @@ public class TaxHistoryConverter implements AttributeConverter<TaxHistory, Strin
     @Override
     public String convertToDatabaseColumn(TaxHistory taxHistory) {
         try {
+            // Convert the TaxHistory to a JSON string.
             return this.objectMapper.writeValueAsString(taxHistory);
         } catch (JsonProcessingException ex) {
+            // If an error occurs, throw a runtime exception to stop the
+            // program from using invalid data.
             final String EXCEPTION_MESSAGE = "Error converting tax history to JSON.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
@@ -31,8 +34,11 @@ public class TaxHistoryConverter implements AttributeConverter<TaxHistory, Strin
     @Override
     public TaxHistory convertToEntityAttribute(String taxHistoryJson) {
         try {
+            // Convert the JSON string to a TaxHistory object.
             return this.objectMapper.readValue(taxHistoryJson, TaxHistory.class);
         } catch (JsonProcessingException ex) {
+            // If an error occurs, throw a runtime exception to stop the
+            // program from using invalid data.
             final String EXCEPTION_MESSAGE = "Error converting JSON to tax history.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
