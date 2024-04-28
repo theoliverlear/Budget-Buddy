42ef2c675f1121faffc24fff25775a56dcbc523e
theoliverlear <sigwartholiver2@gmail.com>
2024-04-28 15:39:59 -0500

    Add contextualizing comments to SavingHistoryConverter.java

--- src/main/java/org/budgetbuddy/convert/entity/holding/saving/SavingHistoryConverter.java
+++ src/main/java/org/budgetbuddy/convert/entity/holding/saving/SavingHistoryConverter.java
@@ -1,6 +1,5 @@
-//=================================-Imports-==================================
 package org.budgetbuddy.convert.entity.holding.saving;
-
+//=================================-Imports-==================================
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
@@ -21,8 +20,11 @@ public class SavingHistoryConverter implements AttributeConverter<SavingHistory,
     @Override
     public String convertToDatabaseColumn(SavingHistory savingHistory) {
         try {
+            // Convert the SavingHistory to a JSON string.
             return this.objectMapper.writeValueAsString(savingHistory);
         } catch (JsonProcessingException ex) {
+            // If an error occurs, throw a runtime exception to stop the
+            // program from using invalid data.
             final String EXCEPTION_MESSAGE = "Error converting saving history to JSON.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
@@ -31,8 +33,11 @@ public class SavingHistoryConverter implements AttributeConverter<SavingHistory,
     @Override
     public SavingHistory convertToEntityAttribute(String savingHistoryJson) {
         try {
+            // Convert the JSON string to a SavingHistory object.
             return this.objectMapper.readValue(savingHistoryJson, SavingHistory.class);
         } catch (JsonProcessingException ex) {
+            // If an error occurs, throw a runtime exception to stop the
+            // program from using invalid data.
             final String EXCEPTION_MESSAGE = "Error converting JSON to saving history.";
             throw new RuntimeException(EXCEPTION_MESSAGE, ex);
         }
