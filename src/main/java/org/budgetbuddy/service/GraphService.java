3be5161ca85d16c1c2e8375ec0cc79fcb3988bfe
theoliverlear <sigwartholiver2@gmail.com>
2024-04-29 12:21:58 -0500

    Add contextualizing comments in GraphService.java

--- src/main/java/org/budgetbuddy/service/GraphService.java
+++ src/main/java/org/budgetbuddy/service/GraphService.java
@@ -15,12 +15,19 @@ public class GraphService {
     //===========================-Constructors-===============================
 
     //=============================-Methods-==================================
+
+    //-------------------------Build-Pie-Graph--------------------------------
     public PieGraph buildPieGraph(Budget userBudget) {
+        // Create a HashMap to store the budget data.
         HashMap<String, Double> budgetData = new HashMap<>();
+        // Get the budget items from the user's budget.
         ArrayList<BudgetItem> budgetItems = userBudget.getBudgetItems();
+        // Iterate through the budget items and add them to the budget data
+        // HashMap.
         for (BudgetItem budgetItem : budgetItems) {
             budgetData.put(budgetItem.getName(), budgetItem.getAmount());
         }
+        // Return a new PieGraph object with the budget data.
         return new PieGraph(budgetData);
     }
     //============================-Overrides-=================================
