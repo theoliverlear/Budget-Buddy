ddf6f74d06c9dcdb667424283e031803c3e478f8
gregdemko <gregory.demko@my.uwrf.edu>
2024-04-29 12:53:18 -0500

    Add test to loadingSpeedTest.js to ensure that web page can load faster than 4 seconds

--- src/test/java/mocha/performance/loadingSpeedTest.js
+++ src/test/java/mocha/performance/loadingSpeedTest.js
@@ -2,3 +2,29 @@ const assert = require('assert');
 const { describe, it } = require('mocha');
 const puppeteer = require('puppeteer');
 const path = require('path');
+
+describe('Home page load speed test', function () {
+
+    describe('home.html load time', function () {
+        it('should load within 4 seconds', async function () {
+            // relative path to home.html
+            const localIndexPagePath = '../../../../main/resources/templates/home.html';
+            const indexPagePath = path.join(__dirname, localIndexPagePath);
+            const pageUrl = `file://${indexPagePath}`;
+
+            // time at which the test started
+            const startTime = Date.now();
+
+            // this waits till page is loaded
+            await testPage.goto(pageUrl, { waitUntil: 'networkidle0' });
+
+            // calculate the website load time in seconds
+            const loadTime = (Date.now() - startTime) / 1000;
+            const loadTimeString = `Load time is ${loadTime} seconds`;
+            console.log(loadTimeString);
+
+            // this checks if the page successfully loads in under 4 seconds
+            assert(loadTime < 4, loadTimeString);
+        });
+    });
+});
